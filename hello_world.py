edcef2776197743df48e482df846e09203aad7fa
Izabella Krystkowiak <izabella.krystkowiak@gmail.com>
2014-10-19 18:27:27 +0100

    Test 2

--- hello_world.py
+++ hello_world.py
@@ -1,3 +1,21 @@
 #!/usr/bin/python
+from optparse import OptionParser
 
-print 'hello world'
+
+def read_flags():
+    '''reads flags from command line'''
+    usage = """usage: %prog [options] args"""
+    parser = OptionParser(usage)
+    parser.add_option("-c", "--count", dest="count", type='int',default=5,help='Optional. Default: 5.')
+    options, args = parser.parse_args()
+    return options
+
+
+
+def hello_world(count=5):
+    for i in xrange(0,count):
+        print 'hello world'
+
+if __name__ == '__main__':
+    options = read_flags()
+    hello_world(options.count)
